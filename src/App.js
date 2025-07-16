65ef082063f3dc113ed127238031b90d7e67c5f9
Tushar Raghav <tushar.raghav@escalesolutions.com>
2025-07-16 11:43:51 +0530

    Fixed React Router v6 routes and cleaned up code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-//import About from './components/About';
+import About from './components/About';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
@@ -37,12 +38,16 @@ function App() {
   }
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert}/>
-      <div className="container my-3">
-        <TextForm showALert={showALert} heading="Enter the text to analyze below" />
-        {/* <About/> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<TextForm showALert={showALert} heading="Enter the text to analyze below" mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
